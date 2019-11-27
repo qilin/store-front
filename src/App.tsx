2183eba53efc8fb70a896e4052b43119525c11f5
ramenskiy <ramenskiy@zdorgor.ru>
2019-11-27 20:09:26 +0800

    init ramblerAuth

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,26 @@ import MainPage from 'pages/MainPage';
 import { isEnvDefined, env } from 'helpers';
 
 const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState('');
+
+  const onInit = (userData: any) => {
+    setUser(userData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    console.log('did mount', (window as any).ramblerIdHelper);
+    init((userData: any) => {
+      onInit(userData);
+    });
+  });
+
+  const openAuth = () => {
+    openAuth();
+  };
+
   if (!isEnvDefined()) {
     return <div>Environment variables is not defined</div>;
   }
