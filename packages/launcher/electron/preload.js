b0d53d620eda3ca4b69cc9cdb7d4d40b802779bb
ramenskiy <ramenskiy.m@gmail.com>
2020-01-24 02:52:58 +0530

    add packages/electron

---
+++ packages/launcher/electron/preload.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ipcRenderer } = require('electron');
+window.ipcRenderer = ipcRenderer;
---
+++ packages/launcher/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'typeface-roboto';
+
+import { App } from '../../shared';
+
+ReactDOM.render(<App />, document.getElementById('root'));
