a8917af340bd11f708c45af4d607eb79b37143fb
ramenskiy <ramenskiy.m@gmail.com>
2020-02-05 18:39:56 +0300

    launcher show version in downloading

--- packages/shared/src/i18n/ru-ru/index.ts
+++ packages/shared/src/i18n/ru-ru/index.ts
@@ -67,7 +67,7 @@ export default {
       download_and_install: 'Скачать и Установить',
     },
     update_status: {
-      DOWNLOAD_UPDATE_PENDING: 'Загрузка обновления',
+      DOWNLOAD_UPDATE_PENDING: 'Загрузка обновления (версия: {{versionToDownload}})',
       UPDATE_AVAILABLE: 'Доступно обновление {{versionToDownload}}',
       UPDATE_NOT_AVAILABLE: 'Обновление для {{currentVersion}} не доступно (Последняя версия: {{versionToDownload}})',
       CHECK_FOR_UPDATE_FAILURE: 'Ошибка поиска обновления, нет доступного обновления',
